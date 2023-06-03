a66438342409ecd80bfe7c5162c70fb714494d99
Scalavia <ihzamahendra4814@gmail.com>
2023-06-04 01:08:57 +0700

    update - new funtion in model ekinerja

--- app/Models/EkinerjaModel.php
+++ app/Models/EkinerjaModel.php
@@ -22,37 +22,37 @@ class EkinerjaModel extends Model
         }
     }
 
-    protected $DBGroup          = 'default';
-    protected $table            = 'ekinerja';
-    protected $primaryKey       = 'id';
+    protected $DBGroup = 'default';
+    protected $table = 'ekinerja';
+    protected $primaryKey = 'id';
     protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = true;
-    protected $protectFields    = true;
+    protected $returnType = 'array';
+    protected $useSoftDeletes = true;
+    protected $protectFields = true;
 
     // Dates
     protected $useTimestamps = true;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
 
     // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
+    protected $validationRules = [];
+    protected $validationMessages = [];
+    protected $skipValidation = false;
     protected $cleanValidationRules = true;
 
     // Callbacks
     protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    protected $beforeInsert = [];
+    protected $afterInsert = [];
+    protected $beforeUpdate = [];
+    protected $afterUpdate = [];
+    protected $beforeFind = [];
+    protected $afterFind = [];
+    protected $beforeDelete = [];
+    protected $afterDelete = [];
 
     public function getDetailKinerja()
     {
@@ -79,5 +79,36 @@ class EkinerjaModel extends Model
     public function deleteEkinerja($id)
     {
         return $this->delete($id);
+    }
+
+    public function getLaporan(array $params = [])
+    {
+        $query = $this->join('users', 'users.id = ekinerja.id_users')
+            ->join('kegiatan', 'kegiatan.id = ekinerja.id_kegiatan')
+            ->select('kegiatan.*, users.*, ekinerja.*')
+            ->where('ekinerja.deleted_at', null);
+
+        if (!empty($params)) {
+            if (!empty($params['nik'])) {
+                $query->where('nik', $params['nik']);
+            }
+
+            if (!empty($params['tanggal'])) {
+                $date_explode = explode('-', $params['tanggal']);
+                if ($params['kategori'] == 'month' || count($date_explode) == 2) {
+                    $query->where('MONTH(ekinerja.created_at)', $date_explode[1])->where('YEAR(ekinerja.created_at)', $date_explode[0]);
+                } else {
+                    $date_explode = explode('/', $params['tanggal']);
+                    if (count($date_explode) == 3) {
+                        $date_explode = $date_explode[2] . '-' . $date_explode[1] . '-' . $date_explode[0];
+                    } else {
+                        $date_explode = $params['tanggal'];
+                    }
+                    $query->where('DATE(ekinerja.created_at)', $date_explode);
+                }
+            }
+        }
+
+        return $query->find();
     }
 }
