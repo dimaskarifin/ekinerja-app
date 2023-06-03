490310a1b1922226423473b6927550f2ec009c97
Scalavia <ihzamahendra4814@gmail.com>
2023-06-04 01:11:14 +0700

    update - profile user

--- app/Controllers/ProfileUser.php
+++ app/Controllers/ProfileUser.php
@@ -3,15 +3,93 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UsersModel;
 
 class ProfileUser extends BaseController
 {
+    protected $user;
+
+    public function __construct()
+    {
+        helper(['form', 'url', 'validation', 'session', 'text']);
+
+        $this->user = new UsersModel;
+    }
     public function index()
     {
         $data = [
-            'title' => 'Profile User'
+            'title' => 'Profile User',
+            'user' => $this->user->where('nik', session('nik'))->first(),
         ];
 
         return view('profile/index', $data);
+    }
+
+    private function setRules($kategori = 'data')
+    {
+        if ($kategori == 'password') {
+            $rules = [
+                'new_password' => [
+                    'label' => "Password",
+                    'rules' => ['required', 'min_length[8]']
+                ],
+                'confirm_password' => [
+                    'label' => "Konfirmasi Password",
+                    'rules' => ['required_with[new_password]', 'matches[new_password]', 'min_length[8]']
+                ]
+            ];
+        } else {
+            $rules = [
+                'nik' => [
+                    'label' => 'NIK',
+                    'rules' => 'required'
+                ],
+                'nama' => [
+                    'label' => 'Nama',
+                    'rules' => 'required'
+                ]
+            ];
+        }
+
+        return $rules;
+    }
+
+    public function update($id)
+    {
+        $input = $this->request->getPost();
+
+        $validation = \Config\Services::validation();
+        
+        if (isset($input['new_password'])) {
+            $validation->setRules($this->setRules('password'));
+            
+            if (!$validation->run($_POST)) {
+                $errors = $validation->getErrors();
+                $arr = implode("<br>", $errors);
+                session()->setFlashdata("warning", $arr);
+                return redirect()->to(base_url('profile-user'));
+            }
+            
+            $this->user->updateUser([
+                'password' => password_hash($input['new_password'], PASSWORD_DEFAULT),
+            ], $id);
+        } else {
+            $validation->setRules($this->setRules());
+
+            if (!$validation->run($_POST)) {
+                $errors = $validation->getErrors();
+                $arr = implode("<br>", $errors);
+                session()->setFlashdata("warning", $arr);
+                return redirect()->to(base_url('profile-user'));
+            }
+    
+            $this->user->updateUser([
+                'nik' => $input['nik'],
+                'nama' => $input['nama'],
+            ], $id);
+        }
+
+        session()->setFlashdata('success', 'Berhasil memperbarui data');
+        return redirect()->to(base_url('profile-user'));
     }
 }
--- app/Views/profile/index.php
+++ app/Views/profile/index.php
@@ -1,5 +1,58 @@
 <?= $this->extend('layouts/index'); ?>
 
 <?= $this->section('main-content'); ?>
+<section class="section">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Profile User</h3>
+                </div>
+                <div class="card-body">
+                    <form action="<?= base_url('profile-user/' . $user['id']) ?>" method="post">
+                        <?= csrf_field() ?>
+                        <div class="form-group">
+                            <label for="">NIK <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" name="nik" value="<?= $user['nik'] ?>" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Nama <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" name="nama" value="<?= $user['nama'] ?>" required>
+                        </div>
+                        <div class="d-grid gap-2 mt-4">
+                            <button type="submit" class="btn btn-success">Perbarui Data</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Perbarui Password</h3>
+                </div>
+                <div class="card-body">
+                    <form action="<?= base_url('profile-user/' . $user['id']) ?>" method="post">
+                        <?= csrf_field() ?>
+                        <div class="form-group">
+                            <label for="">Password <span class="text-danger">*</span></label>
+                            <input type="password" class="form-control" name="new_password" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Konifrmasi Password <span class="text-danger">*</span></label>
+                            <input type="password" class="form-control" name="confirm_password" required>
+                        </div>
+                        <div class="d-grid gap-2 mt-4">
+                            <button type="submit" class="btn btn-warning">Perbarui Password</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?= $this->endSection('main-content'); ?>
 
-<?= $this->endSection(); ?>
+<?= $this->section('script'); ?>
+<?= $this->include('layouts/message-alert'); ?>
+<?= $this->endSection('script'); ?>
