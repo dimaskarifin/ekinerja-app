2584780810455b5860b0acd0c03dc45b9f9fefda
Scalavia <ihzamahendra4814@gmail.com>
2023-06-10 13:12:15 +0700

    update - new kategori minggu

--- app/Controllers/LaporanController.php
+++ app/Controllers/LaporanController.php
@@ -6,6 +6,9 @@ use App\Controllers\BaseController;
 use App\Models\EkinerjaModel;
 use App\Models\UsersModel;
 use CodeIgniter\HTTP\Request;
+use DateInterval;
+use DatePeriod;
+use DateTime;
 use Dompdf\Dompdf;
 
 class LaporanController extends BaseController
@@ -22,11 +25,17 @@ class LaporanController extends BaseController
 
     public function index()
     {
-        $input = $this->request->getGet();
+        $get_data = $this->request->getGet();
+
+        if (!empty($get_data['tanggal'])) {
+            $tanggal = $this->tanggalPerMinggu($get_data['tanggal']);
+            $get_data['date_range']['start_date'] = "$tanggal[0]";
+            $get_data['date_range']['end_date'] = "$tanggal[6]";
+        }
 
         $data = [
             'title' => 'Laporan',
-            'laporans' => $this->ekinerja->getLaporan($input),
+            'laporans' => $this->ekinerja->getLaporan($get_data),
             'users' => $this->user->where('deleted_at', null)->find(),
         ];
 
@@ -36,9 +45,27 @@ class LaporanController extends BaseController
     public function exportPdf()
     {
         $get_data = $this->request->getGet();
-        $kategori = $get_data['kategori'] == 'date' ? 'Harian' : 'Bulanan';
-        $tanggal = $this->tanggalIndo($get_data['tanggal']);
+        $kategori = "";
+        if ($get_data['kategori'] == 'date') {
+            $kategori = "Harian";
+        } else if($get_data['kategori'] == 'week') {
+            $kategori = "Mingguan";
+        } else if ($get_data['kategori'] == 'month') {
+            $kategori = "Bulanan";
+        }
+        
         $tanggal_sekarang = $this->tanggalIndo(date('Y-m-d'));
+        
+        if ($get_data['kategori'] != 'week') {
+            $tanggal = $this->tanggalIndo($get_data['tanggal']);
+        } else {
+            $tanggalPerMinggu = $this->tanggalPerMinggu($get_data['tanggal']);
+            $get_data['date_range']['start_date'] = "$tanggalPerMinggu[0]";
+            $get_data['date_range']['end_date'] = "$tanggalPerMinggu[6]";
+            $tanggalMulai = $this->tanggalIndo($tanggalPerMinggu[0]);
+            $tanggalSelesai = $this->tanggalIndo($tanggalPerMinggu[6]);
+            $tanggal = "{$tanggalMulai} - {$tanggalSelesai}";
+        }
 
         $dompdf = new Dompdf();
 
@@ -61,8 +88,8 @@ class LaporanController extends BaseController
 
     private function tanggalIndo($tanggal)
     {
-        $bulan = array (
-            1 =>   'Januari',
+        $bulan = array(
+            1 => 'Januari',
             'Februari',
             'Maret',
             'April',
@@ -78,9 +105,30 @@ class LaporanController extends BaseController
         $explode = explode('-', $tanggal);
 
         if (count($explode) > 2) {
-            return $explode[2] . ' ' . $bulan[ (int)$explode[1] ] . ' ' . $explode[0];
+            return $explode[2] . ' ' . $bulan[(int) $explode[1]] . ' ' . $explode[0];
         } else {
-            return $bulan[ (int)$explode[1] ] . ' ' . $explode[0];
+            return $bulan[(int) $explode[1]] . ' ' . $explode[0];
         }
+    }
+
+    private function tanggalPerMinggu($tanggal)
+    {   
+        // Membuat objek DateTime dari input
+        $dateTime = new DateTime($tanggal);
+
+        // Mengatur objek DateTime ke hari Senin di minggu yang dipilih
+        $dateTime->modify('this week')->modify('Monday');
+
+        // Mendapatkan tanggal per minggu dengan rentang DatePeriod
+        $dateInterval = new DateInterval('P1D');
+        $dateRange = new DatePeriod($dateTime, $dateInterval, 6);
+
+        // Membuat daftar tanggal per minggu
+        $tanggalPerMinggu = [];
+        foreach ($dateRange as $tanggal) {
+            $tanggalPerMinggu[] = $tanggal->format('Y-m-d');
+        }
+
+        return $tanggalPerMinggu;
     }
 }
